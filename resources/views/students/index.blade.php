7eddfdf70061102c1b496cf0f4b3db9af933377c
Anamcoollzz <hairulanam21@gmail.com>
2024-08-05 17:01:53 +0700

    crud laravel 11

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Belajar CRUD Laravel 11</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <center>
+    <h1>CRUD Laravel 11</h1>
+  </center>
+
+  <div class="container">
+    <table class="table table-bordered table-striped ">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nama</th>
+          <th scope="col">NIM</th>
+          <th scope="col">Tanggal Lahir</th>
+          <th scope="col">Jenis Kelamin</th>
+          <th scope="col">Aksi</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($students as $item)
+          <tr>
+            <th scope="row">{{ $loop->iteration }}</th>
+            <td>{{ $item->name }}</td>
+            <td>{{ $item->nim }}</td>
+            <td>{{ $item->dob }}</td>
+            <td>{{ $item->gender }}</td>
+            <td>
+              <a href="" class="btn btn-primary">Edit</a>
+              <a href="" class="btn btn-danger">Hapus</a>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
