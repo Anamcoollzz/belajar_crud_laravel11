5665f82443f74db0b0a85196ad9bc5b466e519db
Anamcoollzz <hairulanam21@gmail.com>
2024-08-16 20:11:14 +0700

    add datatable

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -7,6 +7,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Belajar CRUD Laravel 11</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.1.4/css/dataTables.bootstrap5.min.css">
 </head>
 
 <body>
@@ -24,7 +25,7 @@
 
     <a href="{{ route('students.create') }}" class="btn btn-primary mb-2">Tambah</a>
 
-    <table class="table table-bordered table-striped ">
+    <table class="table table-bordered table-striped " id="myTable">
       <thead>
         <tr>
           <th scope="col">#</th>
@@ -64,9 +65,14 @@
     @method('DELETE')
   </form>
 
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://cdn.datatables.net/2.1.4/js/dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/2.1.4/js/dataTables.bootstrap5.min.js"></script>
 
   <script>
+    let table = new DataTable('#myTable');
+
     function hapus(e, url) {
       if (confirm('Apakah anda akan menghapus data ini?')) {
         document.getElementById('formHapus').action = url;
